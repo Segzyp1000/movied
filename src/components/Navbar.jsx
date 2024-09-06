7110e9336f1a22619491de8f92e477a796036318
segun olowoyeye <segunolowoyeye@gmail.com>
2024-09-06 05:48:20 +0100

    added search button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { userAuth } from "../context/AuthContext";
+import { FaSearch } from "react-icons/fa";
 
 function Navbar() {
   const { user, logOut } = userAuth();
@@ -16,35 +17,50 @@ function Navbar() {
   };
 
   return (
-    <div className="flex justify-between items-center p-4 z-[100] w-full absolute">
-      <Link to="/">
-        <h1 className="text-red-600 text-3xl font-bold cursor-pointer">
-          NETBOX
-        </h1>
-      </Link>
-      {user?.email ? (
-        <div>
-          <Link to="/account">
-            <button className="text-white pr-4">Account</button>
-          </Link>
-          <Link onClick={handleLogout}>
-            <button className="bg-red-500 text-white px-5 py-3 rounded-lg">
-              Logout
-            </button>
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <Link to="/login">
-            <button className="text-white pr-4">Sign in</button>
-          </Link>
-          <Link to="/signup">
-            <button className="bg-red-500 text-white px-5 py-3 rounded-lg">
-              Sign up
-            </button>
-          </Link>
-        </div>
-      )}
+    <div className="flex flex-wrap justify-between items-center p-4 z-[100] w-full absolute">
+      <div className="flex items-center">
+        <Link to="/">
+          <h1 className="text-red-600 text-3xl font-bold cursor-pointer">
+            NETBOX
+          </h1>
+        </Link>
+      </div>
+      <div className="input-container flex items-center py-3">
+        <input
+          type="text"
+          className="lg:w-full w-[900px] bg-white text-black border-red-500 p-2"
+          placeholder="Search for your favorite movies"
+        />
+
+        <button className="bg-red-600 text-white p-3">
+          <FaSearch />
+        </button>
+      </div>
+      <div className="flex items-center last">
+        {user?.email ? (
+          <div className="ml-4">
+            <Link to="/account">
+              <button className="text-white pr-4">Account</button>
+            </Link>
+            <Link onClick={handleLogout}>
+              <button className="bg-red-500 text-white px-5 py-3 rounded-lg">
+                Logout
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div className="ml-4">
+            <Link to="/login">
+              <button className="text-white pr-4">Sign in</button>
+            </Link>
+            <Link to="/signup">
+              <button className="bg-red-500 text-white px-5 py-3 rounded-lg">
+                Sign up
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
